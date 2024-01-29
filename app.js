227e7a8fbb5782149d8b5a66c68cf142154dda08
Abdulla Hill Baky <ahillbaky@gmail.com>
2024-01-29 20:31:26 +0530

    serverless deploy conf

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-require('dotenv').config()
-const express = require('express')
-const shortid = require('shortid')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const shortid = require("shortid");
+const cors = require("cors");
 
-const { connectDB } = require('./db.js')
-const app = express()
+const { connectDB } = require("./db.js");
+const app = express();
 app.use(
   express.json({
-    limit: '1mb',
-  })
-)
-app.use(cors())
-connectDB()
+    limit: "1mb",
+  }),
+);
+app.use(cors());
+connectDB();
 //route imports
-const urlRoute = require('./router/url.route.js')
-const { Url } = require('./models/url.model.js')
+const urlRoute = require("./router/url.route.js");
+const { Url } = require("./models/url.model.js");
 
-app.use('/url', urlRoute)
+app.use("/url", urlRoute);
+app.get("/", async (req, res) => {
+  res.send("URL Shortner Api ");
+});
 
 //url redirection route
-app.get('/:shortId', async (req, res) => {
+app.get("/:shortId", async (req, res) => {
   try {
-    const shortId = req.params.shortId
+    const shortId = req.params.shortId;
     const entry = await Url.findOneAndUpdate(
       {
         shortId,
@@ -32,15 +35,16 @@ app.get('/:shortId', async (req, res) => {
             timestamps: Date.now(),
           },
         },
-      }
-    )
-    res.redirect(entry?.redirectUrl)
+      },
+    );
+    res.redirect(entry?.redirectUrl);
   } catch (error) {
-    return res.status(500).json({ error })
+    return res.status(500).json({ error });
   }
-})
+});
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 8005;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
+module.exports = app;
--- db.js
+++ db.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(process.env.DB_URI)
-    console.log(`DB connected at host: ${connectionInstance.connection.host}`)
+    const connectionInstance = await mongoose.connect(process.env.DB_URI);
+    console.log(`DB connected at host: ${connectionInstance.connection.host}`);
   } catch (error) {
-    console.log('DB connection Error', error)
-    process.exit(1)
+    console.log("DB connection Error", error);
+    process.exit(1);
   }
-}
+};
 
-module.exports = { connectDB }
+module.exports = { connectDB };
